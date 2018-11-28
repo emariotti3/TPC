4cdb81feaa92ecaacf21ed5080d96515813151c9
sofiamorseletto <sofia.morseletto@mercadolibre.com>
2018-11-28 20:43:59 -0300

    Added parsing method

--- concu-star/src/network/observatory.rs
+++ concu-star/src/network/observatory.rs
@@ -1,13 +1,19 @@
 use std::sync::{Mutex, Arc};
 
 pub struct Observatory {
-    list: Vec<f64>,
-	avg_time: Arc<Mutex<f64>>
+	id:i32,
+    total_time: f64,
+    events: f64,
+	avg_time: Arc<Mutex<f64>>,
+	quadrant_qty: i32,
+	seconds: i32,
+	quadrants_per_server: Vec<i32>
 }
 
 impl Observatory {
-	pub fn new(init_time: Arc<Mutex<f64>>) -> Observatory{
-		Observatory{list:Vec::new(), avg_time: init_time}
+	pub fn new(id:i32, init_time: Arc<Mutex<f64>>, line: &str) -> Observatory{
+		Observatory{total_time:0.0, events:0.0, avg_time: init_time, quadrant_qty:0,seconds:0,quadrants_per_server:Vec::new()};
+		parse_line(line);
 	}
 
 	pub fn get_avg_time(&self) -> f64 {
@@ -16,14 +22,18 @@ impl Observatory {
 	}
 
     pub fn add(&mut self, value: f64) {
-        self.list.push(value);
-        self.update_average();
+        self.total_time += value;
     }
 
 	fn update_average(&mut self) {
-        let total: f64 = self.list.iter().sum();
         let mut time = self.avg_time.lock().unwrap();
-        *time = total as f64 / self.list.len() as f64;
+        *time = self.total_time as f64 / self.events as f64;
+	}
+
+	fn parse_line(String line){
+		//parseo linea y meto los datos en el struct
+		let params: Vec<&str> = line.split(" ").collect();
+		self.quadrant_qty = params[1]; //Pasar a constantes
 	}
 
 
